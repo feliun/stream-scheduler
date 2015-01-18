ae95b106baf8f9f23250f2c94dfdfbb4de2709b7
feliun <felipe.polo@gmail.com>
2015-01-18 01:02:45 +0000

    Adding a sample file to show its usage

---
+++ sample/sample.js
@@ -0,0 +1,31 @@
+var ss = require('../index.js');
+
+module.exports = (function() {
+	var options = {
+		interval: 6, //6 seconds to run the job
+		onStartExec: function() {
+			console.log('Just started!');
+		},
+		onData: function(chunk) {
+			//console.log('Received: ', chunk.toString()); //Comment this out to perceive the rest of the execution
+		},
+		onEnd: function() {
+			console.log('Finished!');
+		},
+		onError: function(err) {
+			console.log('Houston, we\'ve got a problem: ', err);
+		},
+		url: 'http://www.theverge.com/',
+		startNow: true
+	}
+	ss.init(options);
+
+	setTimeout(function() {
+		console.log('Is the job executing?: ', ss.isExecuting());
+	}, 100);
+
+	setTimeout(function() {
+		console.log('The job executed last time at: ', new Date(ss.lastExecutionTime()));
+		ss.startExecution(); //Forcing to execute out of its interval
+	}, 7000)
+})()
