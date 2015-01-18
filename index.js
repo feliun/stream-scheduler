97c6ccd250ea10662a19f94fb3dd82f32813ee82
feliun <felipe.polo@gmail.com>
2015-01-18 00:04:26 +0000

    Including several operations

--- index.js
+++ index.js
@@ -5,8 +5,55 @@ var config = require('./config');
 
 module.exports = function(options) {
 	var interval = options.interval || config.options.interval;
-	var url = options.url;
 	var onData = options.onData || _.noop();
 	var onEnd = options.onEnd || _.noop();
 	var onError = options.onError || _.noop();
+	var url = options.url;
+	if (!url) return onError("A url is required to stream data from");
+	var handler = url.slice(0, 5) === 'https' ? https : http;
+	var isExecuting = false;
+	var lastExecutionTime, lastErrorTime;
+
+	var executeInSeries = function(fnList) { _.each(fnList, function(fn) { fn(); }) }
+	
+	var lockExecution = function() { isExecuting = true; }
+	var unlockExecution = function() { isExecuting = false; }
+	
+	var setLastExecutionTime = function() { lastExecutionTime = new Date().getTime(); }
+	var setLastErrorTime = function() { lastErrorTime = new Date().getTime(); }
+
+	var executeRequest = function() {
+		lockExecution();
+		handler.get(url, function(response) {
+        	response.on('data', executeInSeries([ onData.bind(null, response) ]))
+        			.on('end', executeInSeries([ onEnd, unlockExecution, setLastExecutionTime ]))
+        			.on('error', executeInSeries([ onError, unlockExecution, setLastErrorTime ]));
+		}
+	}
+
+	if (options.startNow) executeRequest();
+	setInterval(executeRequest, interval);
+
+	function isExecuting() { 
+		return isExecuting; 
+	}
+
+	function lastExecutionTime() {
+		return lastExecutionTime;
+	}
+	
+	function startExecution() {
+		if (!isExecuting) executeRequest();
+	}
+
+	function stopExecution() {
+
+	}
+
+	return {
+		isExecuting: isExecuting,
+		startExecution: startExecution,
+		stopExecution: stopExecution,
+		lastExecutionTime: lastExecutionTime
+	}
 }
